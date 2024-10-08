a789f5ea0474b32d42d5ea8ae848dc151a2cf284
AnhTuanDELL <Phananhtuan6@outlook.com>
2024-10-08 11:47:32 +0700

    Test 2

--- TestGitHub/Form2.Designer.cs
+++ TestGitHub/Form2.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(325, 200);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.Size = new System.Drawing.Size(84, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "TEST";
+            this.label1.Text = "TEST 2";
             // 
             // Form2
             // 
